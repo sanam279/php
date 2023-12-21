a8459ba7f35f81afb5e0fcfb50281e56543d896d
sanam279 <132648663+sanam279@users.noreply.github.com>
2023-12-21 11:44:53 +0530

    Add files via upload

---
+++ get-token.php
@@ -0,0 +1,22 @@
+<?php
+$data = 'username=kpmgp-da9&password=aP4_K/VSPHq5&token=true';
+
+//$json = json_encode($data);
+$url = 'https://gateway.qg1.apps.qualys.eu/auth';
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/x-www-form-urlencoded',
+));
+
+$response = curl_exec($ch);
+if(curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    echo $response;
+}
+curl_close($ch);
+?>
